54cdbf46a3ef8e49056f9ebf2abbdef624e108a0
Joseph Pancho <31480952+janchpanch@users.noreply.github.com>
2021-10-26 13:24:47 -0500

    Add files via upload

--- LuhnCheck.java
+++ LuhnCheck.java
@@ -16,13 +16,63 @@ public class LuhnCheck {
         input = sc.nextLong();
 
         stringInput = Long.toString(input);
-
-    
+        System.out.println(rToLEvenSum(input));
+        System.out.println(rToLOddSum(input));
     }
 
-    
+    public static long rToLEvenSum(long input){
+        long output = 0,
+             digit = 0,
+             tracker = 1,
+             numX2 = 0;
+        
+        while (input > 0) {    
+            if ((tracker % 2) == 0){
+                digit = input % 10;
+                numX2 = digit * 2;
 
-    static boolean cardIsValid(String input){
+                if (numX2 < 10){
+                    output += digit * 2;
+                }
+                else if (numX2 > 9){
+                    output += numX2 - 9;
+                }
+               
+                input /= 10;
+                tracker++;
+            }
+            else if ((tracker % 2) != 0) {
+                //temp = input % 10;
+                input /= 10;
+                tracker++;
+            }
+        }
+        return output;
+    }
+
+    public static long rToLOddSum(long input) {
+        long sum = 0, 
+             digit = 0, 
+             tracker = 1;
+
+        while (input > 1) {
+            if ((tracker % 2) != 0) {
+
+                digit = input % 10;
+                sum += digit;
+                input /= 10;
+                tracker++;
+            } 
+            
+            else if ((tracker % 2) == 0) {
+                input /= 10;
+                tracker++;
+            }
+        }
+        return sum;
+    }
+
+    public static boolean isCardLengthCorrect(String input){
         if (input.length() >= 13 && input.length() <= 16){
             System.out.println("Card is valid");
             return true;
@@ -33,26 +83,29 @@ public class LuhnCheck {
         }
     }
 
-    static String cardType(String input){
+    public static boolean isPrefixCorrect(String input){
 
-        String output = new String();
+        boolean output = false;
 
         if (input.charAt(0) == 4){
-            output = VISA;
+            output = true;
         }
 
         else if (input.charAt(0) == 5) {
-            output = MASTER_CARD;
+            output = true;
         }
 
         else if (input.charAt(0) == 6) {
-            output = DISCOVER;
+            output = true;
         }
 
         else if (input.charAt(0) == 3){
             if(input.charAt(1) == 7){
-                output = AMEX;
+                output = true;
             }
+        }
+        else {
+            output = false;
         }
         return output;
     }
