ac755739308bf02e7c4e2047817c241783296da5
Joseph Pancho <31480952+janchpanch@users.noreply.github.com>
2021-10-26 23:18:42 -0500

    Add files via upload

--- LuhnCheck.java
+++ LuhnCheck.java
@@ -9,71 +9,67 @@ public class LuhnCheck {
 
         System.out.println("Input a credit card number: ");
         input = sc.nextLong();
-
         stringInput = Long.toString(input);
+
         if (isValid(input, stringInput)) {
             System.out.print(input + " is valid");
-        }
-        else if (!isValid(input, stringInput)){
+        } 
+        else if (!isValid(input, stringInput)) {
             System.out.print(input + " is not valid");
         }
         sc.close();
     }
+
+    // Holistic check function for card validity 
     public static boolean isValid(Long longInput, String stringInput) {
+        // Sum the values of each summation function and take the mod 10
         long sum = (rToLEvenSum(longInput) + rToLOddSum(longInput)) % 10;
-        boolean output = false; 
+        boolean output = false;
+        
+        // If all conditions all satisfied, card is valid
         if (sum == 0 && isCardLengthCorrect(stringInput) && isPrefixCorrect(stringInput)) {
             output = true;
-        }
-        else if((sum != 0) || !isCardLengthCorrect(stringInput) || !isPrefixCorrect(stringInput)) {
+        } 
+        
+        // If even one condition fails, card is invalid
+        else if ((sum != 0) || !isCardLengthCorrect(stringInput) || !isPrefixCorrect(stringInput)) {
             output = false;
         }
         return output;
     }
-    public static long rToLEvenSum(long input){
-        long output = 0,
-             digit = 0,
-             tracker = 1,
+
+    // Sum the doubles of the evens
+    public static long rToLEvenSum(long input) {
+        long sum = 0, 
+             digit = 0, 
+             tracker = 1, 
              numX2 = 0;
 
-        while (input > 0) {    
-            if ((tracker % 2) == 0){
+        // Uses tracker to iterage through odds or evens independent of input
+        while (input > 0) {
+            if ((tracker % 2) == 0) {
+                
+                // Store the current digit's value
                 digit = input % 10;
                 numX2 = digit * 2;
 
-                if (numX2 < 10){
-                    output += digit * 2;
+                // Add to sum if * 2 is < 10
+                if (numX2 < 10) {
+                    sum += digit * 2;
+                } 
+                
+                // Add digits together if * 2 is > 10
+                // Subtracting 9 is the same is adding digits 
+                else if (numX2 > 9) {
+                    sum += numX2 - 9;
                 }
-                else if (numX2 > 9){
-                    output += numX2 - 9;
-                }
 
-                input /= 10;
-                tracker++;
-            }
-            else if ((tracker % 2) != 0) {
-                input /= 10;
-                tracker++;
-            }
-        }
-        return output;
-    }
-
-    public static long rToLOddSum(long input) {
-        long sum = 0, 
-             digit = 0, 
-             tracker = 1;
-
-        while (input > 1) {
-            if ((tracker % 2) != 0) {
-
-                digit = input % 10;
-                sum += digit;
+                // Clip current digit
                 input /= 10;
                 tracker++;
             } 
-
-            else if ((tracker % 2) == 0) {
+            else if ((tracker % 2) != 0) {
+                // Clip current digit without changing sum
                 input /= 10;
                 tracker++;
             }
@@ -81,20 +77,50 @@ public class LuhnCheck {
         return sum;
     }
 
-    public static boolean isCardLengthCorrect(String input){
-        if (input.length() >= 13 && input.length() <= 16){
-            return true;
+    // Sum the odds
+    public static long rToLOddSum(long input) {
+        long sum = 0, 
+             digit = 0, 
+             tracker = 1;
+
+        // Uses tracker to iterage through odds or evens independent of input
+        while (input > 1) {
+            if ((tracker % 2) != 0) {
+
+                // Store the current digit's value
+                digit = input % 10;
+                
+                // Add to sum
+                sum += digit;
+
+                // Clip current digit
+                input /= 10;
+                tracker++;
+            }
+
+            else if ((tracker % 2) == 0) {
+                // Clip current digit without changing sum
+                input /= 10;
+                tracker++;
+            }
         }
-        else{
+        return sum;
+    }
+
+    // Checks card length
+    public static boolean isCardLengthCorrect(String input) {
+        if (input.length() >= 13 && input.length() <= 16) {
+            return true;
+        } else {
             return false;
         }
     }
 
-    public static boolean isPrefixCorrect(String input){
-
+    public static boolean isPrefixCorrect(String input) {
         boolean output = false;
 
-        if (Integer.parseInt(Character.toString(input.charAt(0))) == 4){
+        // Visa
+        if (Integer.parseInt(Character.toString(input.charAt(0))) == 4) {
             output = true;
         }
 
@@ -109,10 +135,14 @@ public class LuhnCheck {
         }
 
         // American Express
-        else if (Integer.parseInt(Character.toString(input.charAt(0))) == 3){
-            if(Integer.parseInt(Character.toString(input.charAt(1))) == 7){
+        else if (Integer.parseInt(Character.toString(input.charAt(0))) == 3) {
+            if (Integer.parseInt(Character.toString(input.charAt(1))) == 7) {
                 output = true;
             }
+        }
+
+        else {
+            output = false;
         }
         return output;
     }
